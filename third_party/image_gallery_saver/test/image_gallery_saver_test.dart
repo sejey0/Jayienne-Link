1d921f4a62f106cf2818562c8ab4a3efb362fe41
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2026-04-20 14:53:51 +0800

    feat: Add image_gallery_saver plugin with iOS and example implementation
    - Created the image_gallery_saver plugin to save images and videos to the gallery.
    - Implemented iOS platform code in Swift and Objective-C.
    - Added example Flutter app demonstrating the usage of the plugin.
    - Included unit tests for the plugin functionality.
    - Updated pubspec.yaml and podspec files for proper configuration.

---
+++ third_party/image_gallery_saver/test/image_gallery_saver_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+void main() {
+  final TestWidgetsFlutterBinding binding =
+      TestWidgetsFlutterBinding.ensureInitialized();
+
+  const MethodChannel channel = MethodChannel('image_gallery_saver');
+  final List<MethodCall> log = <MethodCall>[];
+  bool? response;
+
+  binding.defaultBinaryMessenger.setMockMethodCallHandler(
+    channel,
+    (MethodCall methodCall) async {
+      log.add(methodCall);
+      return response;
+    },
+  );
+
+  tearDown(() {
+    log.clear();
+    binding.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+  });
+
+  test('saveImageToGallery test', () async {
+    response = true;
+    Uint8List imageBytes = Uint8List(16);
+    final bool? result = await ImageGallerySaver.saveImage(imageBytes);
+    expect(
+      log,
+      <Matcher>[
+        isMethodCall('saveImageToGallery', arguments: <String, dynamic>{
+          'imageBytes': imageBytes,
+          'quality': 80,
+          'name': null,
+          "isReturnImagePathOfIOS": false
+        })
+      ],
+    );
+    expect(result, response);
+  });
+}
